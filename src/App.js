e09357c2bfe916aa37757487a37ae061aeb68538
mrlim0802 <sjlim0802@gmail.com>
2023-03-11 23:09:25 -0500

    added NotFound.js for 404 screen, updated BlogDetails.js to hadle delete request

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -19,7 +20,11 @@ function App() {
             </Route>
             <Route path='/blogs/:id'>
               <BlogDetails /> 
-            </Route>      
+            </Route>
+            <Route path='*'>
+              <NotFound /> 
+            </Route>
+
           </Switch>
         </div>    
       </div>
--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -2,17 +2,17 @@ import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-const { id } = useParams();
-const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
-const history = useHistory(); 
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
+    const history = useHistory(); 
 
-const handleClick = () => {
-    fetch(`http://localhost:8000/blogs/${blog.id}`, {
-        method: 'DELETE'
-    }).then(() => {
-        history.push('/');
-    })
-}
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${blog.id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="blog-details">
