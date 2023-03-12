a228f9ff2855df5a1a992002d3049cf4929ee896
mrlim0802 <sjlim0802@gmail.com>
2023-03-11 23:24:38 -0500

    removed setTimeout option from useFetch for instant loading and a better user experience

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('');
+    const [author, setAuthor] = useState('Johnny');
     const [isPending, setIsPending] = useState(false);
     const history = useHistory();
 
@@ -12,6 +12,7 @@ const Create = () => {
         e.preventDefault();
         const blog = { title, body, author };
         setIsPending(true);
+        
         fetch('http://localhost:8000/blogs', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -29,18 +30,9 @@ const Create = () => {
             <h2>Add a new blog</h2>
             <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
-                <input 
-                    type="text"
-                    required
-                    value={ title }
-                    onChange={(e) => setTitle(e.target.value)}
-                />
+                <input type="text" required value={ title }onChange={(e) => setTitle(e.target.value)}/>
                 <label>Blog Body:</label>
-                <textarea
-                    required
-                    value={ body }
-                    onChange={(e) => setBody(e.target.value)}
-                ></textarea>
+                <textarea required value={ body } onChange={(e) => setBody(e.target.value)}></textarea>
                 <label>Blog Author:</label>
                 <select value={ author } onChange={(e) => setAuthor(e.target.value)}>
                     <option value="johnny">Johnny</option>
