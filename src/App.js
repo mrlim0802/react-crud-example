77c7b1bdc94d6403bffc09080b6f6b19889f8374
mrlim0802 <sjlim0802@gmail.com>
2023-03-11 23:08:04 -0500

    added functionality to blog, created a useFetch enpont to the db, BlogList.js and BlogDetails.js fuctionality is now saved on the db and deleted at will, 404 Notfound set up under NotFound.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -9,9 +11,15 @@ function App() {
         <Navbar />
         <div className='content'>
           <Switch>
-            <Route path='/'>
+            <Route exact path='/'>
               <Home /> 
             </Route>
+            <Route path='/create'>
+              <Create /> 
+            </Route>
+            <Route path='/blogs/:id'>
+              <BlogDetails /> 
+            </Route>      
           </Switch>
         </div>    
       </div>
